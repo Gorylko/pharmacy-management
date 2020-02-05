41f5f2c9889471ca17e7acbd6bedfc5153fc721b
gorylko <shamyachka@gmail.com>
2020-02-05 23:30:58 +0300

    added med class + references

---
+++ App/PharmacyManager.Shared/Entities/Medicament.cs
@@ -0,0 +1,11 @@
+﻿namespace PharmacyManager.Shared.Entities
+{
+    public class Medicament
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Manufacturer { get; set; }
+    }
+}
